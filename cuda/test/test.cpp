07f46089bc933dbd9c8355171ad34d10ea74c517
Adam Šmelko <adamsmelko1@gmail.com>
2023-02-23 17:30:25 +0100

    add table tests

--- cuda/test/test.cpp
+++ cuda/test/test.cpp
@@ -1,4 +1,6 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <thrust/host_vector.h>
 
 #include "transition_table.cuh"
 
@@ -19,4 +21,10 @@ TEST(trans_table, toy)
 	transition_table table(context, std::move(model));
 
 	table.construct_table();
+
+	thrust::host_vector<index_t> indptr = table.indptr;
+	thrust::host_vector<index_t> indices = table.indices;
+
+	ASSERT_THAT(indptr, ::testing::ElementsAre(0, 0, 2, 4, 5, 7, 8, 8, 8));
+	ASSERT_THAT(indices, ::testing::ElementsAre(3, 5, 0, 6, 7, 0, 6, 7));
 }
