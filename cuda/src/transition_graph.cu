9531db0eda5af779baa780cc70934a1c13565de5
Adam Smelko <adamsmelko1@gmail.com>
2023-02-26 18:12:13 +0100

    wip

--- cuda/src/transition_graph.cu
+++ cuda/src/transition_graph.cu
@@ -2,17 +2,22 @@
 #include "transition_graph.h"
 #include "utils.cuh"
 
-transition_graph::transition_graph(d_idxvec csr_indices, d_idxvec csr_indptr)
-	: indices_(std::move(csr_indices)), indptr_(std::move(csr_indptr)), vertices_count_(indptr_.size() - 1)
+transition_graph::transition_graph(d_idxvec csr_indices, d_idxvec csr_indptr, d_idxvec rows, d_idxvec cols)
+	: indices_(std::move(csr_indices)), indptr_(std::move(csr_indptr)), rows_(std::move(rows)), cols_(std::move(cols)), vertices_count_(indptr_.size() - 1)
 {}
 
 void transition_graph::order_vertices()
 {
-	d_idxvec labels(indptr_.size() - 1);
+	d_idxvec labels(vertices_count_);
 
-	SCC_Data<char, int> sccd(indptr_.size() - 1, indptr_.data().get(), indices_.data().get());
-	auto scc_count = sccd.run_scc(labels.data().get());
+	SCC_Data<char, int> sccd(vertices_count_, indptr_.data().get(), indices_.data().get());
+	sccd.run_scc(labels.data().get());
 
-	std::cout << scc_count << std::endl;
 	print("labels: ", labels);
+
+	auto in_begin = thrust::make_zip_iterator(rows_.begin(), cols_.begin());
+	auto in_end = thrust::make_zip_iterator(rows_.end(), cols_.end());
+
+	thrust::transform_if(in_begin, in_end, )
+
 }
--- cuda/src/transition_graph.h
+++ cuda/src/transition_graph.h
@@ -4,12 +4,12 @@
 
 class transition_graph
 {
-	d_idxvec indices_, indptr_;
+	d_idxvec indices_, indptr_, rows_, cols_;
 
 	size_t vertices_count_;
 
 public:
-	transition_graph(d_idxvec csr_indices, d_idxvec csr_indptr);
+	transition_graph(d_idxvec csr_indices, d_idxvec csr_indptr, d_idxvec rows, d_idxvec cols);
 
     void order_vertices();
 };
