0e32605f359bda442078a6150f3bba2d2d34adc2
Adam Smelko <adamsmelko1@gmail.com>
2023-03-24 17:40:36 +0100

    revert timer

--- cuda/src/sga/timer.h
+++ cuda/src/sga/timer.h
@@ -4,6 +4,8 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+#include <sys/time.h>
+
 
 /*
 GAP Benchmark Suite
@@ -19,22 +21,30 @@ class Timer {
   Timer() {}
 
   void Start() {
+    gettimeofday(&start_time_, NULL);
   }
 
   void Stop() {
+    gettimeofday(&elapsed_time_, NULL);
+    elapsed_time_.tv_sec  -= start_time_.tv_sec;
+    elapsed_time_.tv_usec -= start_time_.tv_usec;
   }
 
   double Seconds() const {
-    return 0.;
+    return elapsed_time_.tv_sec + (double)elapsed_time_.tv_usec/1e6;
   }
 
-  double Millisecs() const { return 0.;
+  double Millisecs() const {
+    return 1000*elapsed_time_.tv_sec + (double)elapsed_time_.tv_usec/1000;
   }
 
-  double Microsecs() const { return 0.;
+  double Microsecs() const {
+    return 1e6*elapsed_time_.tv_sec + (double)elapsed_time_.tv_usec;
   }
 
  private:
+  struct timeval start_time_;
+  struct timeval elapsed_time_;
 };
 
 // Times op's execution using the timer t
