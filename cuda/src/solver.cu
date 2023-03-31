f73174f47ff36a8c29a78cf52c524a885cf34181
Adam Šmelko <adamsmelko1@gmail.com>
2023-03-31 21:59:04 +0200

    wip

--- cuda/src/solver.cu
+++ cuda/src/solver.cu
@@ -55,7 +55,9 @@ __global__ void scatter_rows_data(const index_t* __restrict__ dst_indptr, index_
 		}
 
 		bool up = r < diag;
-		index_t state = __ffs(r ^ diag);
+		index_t state = __ffs(r ^ diag) - 1;
+		if (state == -1)
+			printf("bad\n");
 		real_t rate = rates[2 * state + (up ? 0 : 1)];
 		printf("rate %f idx % i\n", rate, 2 * state + (up ? 0 : 1));
 		dst_data[dst_begin + i] = rate;
