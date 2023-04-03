aa690645bcfae4b457e62c3c286ad1e6b11bf89c
Adam Smelko <adamsmelko1@gmail.com>
2023-04-03 17:32:14 +0200

    better logs

--- cuda/src/utils.h
+++ cuda/src/utils.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
-#include <thrust/device_vector.h>
-#include <thrust/host_vector.h>
+#include "sparse_utils.h"
 
 template <typename T>
 void print(const char* msg, const thrust::device_vector<T>& v, size_t count = 0)
@@ -15,4 +15,20 @@ void print(const char* msg, const thrust::device_vector<T>& v, size_t count = 0)
 	for (auto it = h.begin(); it < end; it++)
 		std::cout << *it << " ";
 	std::cout << std::endl;
+}
+
+template <typename T1, typename T2>
+void print(const char* msg, T1& indptr, T1& indices, T2& data, size_t count = 0)
+{
+	print(std::string(msg) + " indptr  ", indptr, count);
+	print(std::string(msg) + " indices ", indices, count);
+	print(std::string(msg) + " data    ", data, count);
+}
+
+template <cs_kind k, typename... Args>
+void print(const char* msg, sparse_cs_matrix<k, Args...>& m, size_t count = 0)
+{
+	print(std::string(msg) + " indptr  ", m.indptr, count);
+	print(std::string(msg) + " indices ", m.indices, count);
+	print(std::string(msg) + " data    ", m.data, count);
 }
